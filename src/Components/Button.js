cd782c62de3a3dc107fe29a0df8b6e6e167c65f0
caarlosdamian <dapecarlos@gmail.com>
2021-06-29 10:30:39 -0500

    initialize application

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Button = () => {
+    const [showBotton, setShowBotton] = useState(false)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Button
